fb869a47bb812d122314faf3601fc03259a03074
Yejun Su <yejun@hey.com>
2023-09-01 09:20:32 +0800

    add rails_postgres template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
         path = ./direnv;
         description = "A direnv template using the latest stable nixpkgs";
       };
+      rails_postgres = {
+        path = ./rails_postgres;
+        description =
+          "A devenv template for developing Rails + PostgreSQL application";
+      };
     };
 
     defaultTemplate = self.templates.direnv;
